a64520d1c32ce22d4fda83fcb63a7387b4db46e2
dom1torii <dom1torii@proton.me>
2023-06-01 03:36:23 +0300

    fixed getskins

--- commands/fun/getskins.js
+++ commands/fun/getskins.js
@@ -9,33 +9,31 @@ module.exports = {
 		    option.setName('weapon')
 			    .setDescription('Weapon name')),
     async execute(interaction) {
-        if (interaction.member.roles.cache.has('1113017259875651594')) {
-            const weapon = interaction.options.getString("weapon")
+        const weapon = interaction.options.getString("weapon")
 
-            let response = ""
+        let response = ""
 
-            await fetch('https://valorant-api.com/v1/weapons', {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                },
-            })
-            .then(data => data.json())
-            .then(data => {
-                for (i=0; i<data["data"].length; i++) {
-                    if (data["data"][i]["displayName"] == weapon) {
-                        for (j=0; j<data["data"][i]["skins"].length; j++) {
-                            response += JSON.stringify(data["data"][i]["skins"][j]["displayName"]) + "\n"
-                        }
-
+        await fetch('https://valorant-api.com/v1/weapons', {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+            },
+        })
+        .then(data => data.json())
+        .then(data => {
+            for (i=0; i<data["data"].length; i++) {
+                if (data["data"][i]["displayName"] == weapon) {
+                    for (j=0; j<data["data"][i]["skins"].length; j++) {
+                        response += JSON.stringify(data["data"][i]["skins"][j]["displayName"]) + "\n"
                     }
+
                 }
             }
+        }
 
-            )
-            if (response.length>0) {
-                await interaction.reply(response)
-            }
+        )
+        if (response.length>0) {
+            await interaction.reply(response)
         }
 
     }
